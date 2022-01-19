7ba70696b85cd9f7e7693b4173c91593ec0267fe
shreyans <shreyanz32@gmail.com>
2022-01-19 23:11:40 +0530

    thirteenth commit

--- recette/src/components/NavBar.js
+++ recette/src/components/NavBar.js
@@ -18,17 +18,11 @@ function NavBar() {
             </Navbar>
 
             <section class="text-gray-600 body-font">
-              <div class="container px-5 py-24 mx-auto flex flex-wrap">
+              <div class="container px-5 py-24 mx-auto flex flex-wrap w-full h-full">
                 <h2 class="sm:text-3xl text-2xl text-gray-900 font-medium title-font mb-2 md:w-2/5">The final destination to save your basket.</h2>
                 <div class="md:w-3/5 md:pl-6">
-                  <p class="leading-relaxed text-base">The world is very concerned with eating clean and green these days. Intermittent fasting, Veganism and keto diet are terms we have all heard and maybe even tried. But a lot of these need time effort and most importantly resources.
-	
-	Food waste is still a big concern. The fast lifestyles make people buy in bulk and get things fast and easy. With food and grocery delivery services it is even simpler to just get stuff on hands. So, the value of what we have is reduced greatly and we just throw things that are old and expired. All the resource and energy consumption will contribute to leaving a carbon footprint on the planet
-	
-	With RECETTE you can keep track of your groceries and make sure to use them up before they go out of date. Keep adding in whatever you purchase and have it all in one place. It is easy to use on all your devices too. 
-	
-	Happy Eating!!!</p>
-                  <p class="leading-relaxed text-base">Nathani Juhi | Dahikar Sneha | Jaiswal Ved | Jain Shreyans</p>
+                  <p class="leading-relaxed text-base">Keep track of your inventory, all at one single place. This application will aid you in your journey to become a better human.</p>
+                  <p class="leading-relaxed text-base"><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>Nathani Juhi | Dahikar Sneha | Jaiswal Ved | Jain Shreyans</p>
                   <div class="flex md:mt-4 mt-6">
                     <a href='#666' class='no-underline mx-auto'>
                       <button class="inline-flex text-black font-semibold bg-emerald-300 border-0 py-1 px-4 focus:outline-none hover:bg-emerald-400 rounded">Go to inventory.</button>
