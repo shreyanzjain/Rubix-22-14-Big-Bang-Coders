079b6c9731790fafdd6e61a882670888ae75f126
shreyans <shreyanz32@gmail.com>
2022-01-20 02:08:56 +0530

    sixteen.one - minor font

--- recette/src/components/NavBar.js
+++ recette/src/components/NavBar.js
@@ -22,8 +22,8 @@ function NavBar() {
                 <h2 class="sm:text-3xl text-2xl text-gray-900 font-medium title-font mb-2 md:w-2/5">The final destination to save your basket.</h2>
                 <div class="md:w-3/5 md:pl-6">
                   <p class="leading-relaxed text-base italic text-xl"><br/><br/><br/>Keep track of your inventory, all at one single place. This application will aid you in your journey to become a better human.</p>
-                  <p class="leading-relaxed text-base"><br/><br/><br/><br/><br/><br/><br/>Nathani Juhi | Dahikar Sneha | Jaiswal Ved | Jain Shreyans</p>
-                  <div class="flex md:mt-4 mt-6">
+                  <p class="leading-relaxed text-base"><br/><br/><br/>Nathani Juhi | Dahikar Sneha | Jaiswal Ved | Jain Shreyans</p>
+                  <div class="flex md:mt-4 my-16">
                     <a href='#666' class='no-underline mx-auto'>
                       <button class="inline-flex text-black font-semibold bg-emerald-300 border-0 py-1 px-4 focus:outline-none hover:bg-emerald-400 rounded">Go to inventory.</button>
                       </a>    
